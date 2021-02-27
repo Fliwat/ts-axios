5839213ca66596b6ad797e13cc98181b36d171f5
Fliwat <20272981+Fliwat@users.noreply.github.com>
2021-02-27 17:23:38 +0800

    feat: init example

---
+++ examples/webpack.config.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+
+const entry = fs.readdirSync(__dirname).reduce((entries, dir) => {
+  const fullDir = path.join(__dirname, dir)
+  const entry = path.join(fullDir, 'app.ts')
+
+  if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
+    entries[dir] = ['webpack-hot-middleware/client', entry]
+  }
+
+  return entries
+}, {})
+
+module.exports = {
+  mode: 'development',
+
+  entry,
+
+  output: {
+    path: path.join(__dirname, '__build__'),
+    filename: '[name].js',
+    publicPath: '/__build__/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'tslint-loader'
+          }
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+}
---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+import axios from '..'
+
+type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
