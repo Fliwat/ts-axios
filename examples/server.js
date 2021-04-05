e34dea5858dd3ed53ed8bca2c81f69a5b2eaf683
Fliwat <20272981+Fliwat@users.noreply.github.com>
2021-04-05 18:46:48 +0800

    feat(config): add default config

--- examples/server.js
+++ examples/server.js
@@ -35,6 +35,8 @@ registerExtendRouter()
 
 registerInterceptorRouter()
 
+registerConfigRouter()
+
 app.use(router)
 
 const port = process.env.PORT || 9000
@@ -142,5 +144,11 @@ function registerExtendRouter() {
 function registerInterceptorRouter() {
   router.get('/interceptor/get', function(req, res) {
     res.end('hello')
+  })
+}
+
+function registerConfigRouter() {
+  router.post('/config/post', function(req, res) {
+    res.json(req.body)
   })
 }
