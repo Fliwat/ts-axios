345b3020fec9a8be1a41f00014d7f543fdc38ce9
Fliwat <20272981+Fliwat@users.noreply.github.com>
2021-04-05 15:55:09 +0800

    feat(interceptor): add interceptor

--- examples/server.js
+++ examples/server.js
@@ -33,6 +33,8 @@ registerErrorRouter()
 
 registerExtendRouter()
 
+registerInterceptorRouter()
+
 app.use(router)
 
 const port = process.env.PORT || 9000
@@ -134,5 +136,11 @@ function registerExtendRouter() {
         age: 18
       }
     })
+  })
+}
+
+function registerInterceptorRouter() {
+  router.get('/interceptor/get', function(req, res) {
+    res.end('hello')
   })
 }
